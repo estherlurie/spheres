1633f4552abc1158d22f1bc19b46cded3fb6a3fd
Esti Lurie <estilurie@gmail.com>
2024-09-18 19:59:14 -0400

    update encryptpassword

--- src/lib/server/encrypt.ts
+++ src/lib/server/encrypt.ts
@@ -1,14 +1,19 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-const encryptPassword = async (username: string, password: string, callback: Function) => {
-  return await bcrypt.hash(password, 10, async (err, hash) => {
+type CallbackFunction = (hashedPassword: string) => void;
+
+const encryptPassword = async (
+  username: string,
+  password: string,
+  callback: CallbackFunction
+): Promise<void> => {
+  bcrypt.hash(password, 10, async (err, hash) => {
     if (err) {
-      console.log("Error hashing: " + err);
-      return err;
+      console.log("encryptPassword: Error hashing: " + err);
+    } else {
+      await callback(hash);
     }
-    await callback(hash);
   });
 };
 
 export { encryptPassword };
-
