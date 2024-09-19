deb0212c1b8675dc4f1a182c8603a273290e305a
Esti Lurie <estilurie@gmail.com>
2024-09-19 00:01:05 -0400

    correctly sign in

--- src/routes/login/signin/+page.server.ts
+++ src/routes/login/signin/+page.server.ts
@@ -1,33 +1,45 @@
-import type { Actions } from '@sveltejs/kit';
-import { prisma } from '$lib/server/prisma';
-import { encryptPassword } from '$lib/server/encrypt';
-import { redirect } from '@sveltejs/kit';
+import type { Actions } from "@sveltejs/kit";
+import bcrypt from "bcrypt";
+import { prisma } from "$lib/server/prisma";
+import { encryptPassword } from "$lib/server/encrypt";
+import { redirect } from "@sveltejs/kit";
+
+function log(message: string) {
+  console.log("/login/signin: " + message);
+}
 
 export const actions: Actions = {
-	signIn: async ({ cookies, request }) => {
-		console.log("sign in action");
-		let formData = await request.formData();
-		const { username, password } = Object.fromEntries(formData) as {
-			username: string;
-			password: string;
-		};
-		console.log("Attempt sign in for user: " + username);
-		await encryptPassword(username, password, async (passwordHash: string) => {
-			let user = await prisma.spheres_users.findFirst({
-				where: {
-					name: username,
-					password: passwordHash
-				}
-			});
-			if (user) {
-				console.log("Logging in");
-				redirect(302, 'login/signin/success');
-			} else {
-				console.log("Incorrect username or password");
-				return {
-					success: false, error: "Incorrect username or password"
-				};
-			}
-		});
-	}
-}
+  signIn: async ({ cookies, request }) => {
+    let formData = await request.formData();
+    const { username, password } = Object.fromEntries(formData) as {
+      username: string;
+      password: string;
+    };
+
+    log("Attempt sign in for user: " + username);
+
+    let user = await prisma.spheres_users.findFirst({
+      select: {
+        id: true,
+        name: true,
+        password: true,
+      },
+      where: {
+        name: username,
+      },
+    });
+    if (!user) {
+      let error = "Couldn't find user=" + username + " in db";
+      log(error);
+      return { status: 500, message: error };
+    }
+    let ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+      let error = "Incorrect password";
+      log(error);
+      return { status: 500, message: error };
+    } else {
+      redirect(302, "/login/success/" + user.id);
+    }
+  },
+};
